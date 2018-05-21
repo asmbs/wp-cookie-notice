ab7c8a8f9883b8e970df1843151de91c10de7099
Max McMahon <max@asmbs.org>
2018-05-21 11:56:42 -0400

    Add JS documentation

--- js/src/main.js
+++ js/src/main.js
@@ -1,14 +1,23 @@
 import '../../css/src/style.scss';
 import { CookieManager } from './cookie-manager.js';
 
+/**
+ * A class the cookie notice functionality.
+ */
 export class CookieNotice {
 
+    /**
+     * Constructor.
+     */
     constructor() {
         this.noticeContainer = $( '#wpcn_container' );
         this.cookieManager = new CookieManager();
         this.cookieName = 'wpcn_notice_accepted';
     };
 
+    /**
+     * Initializes all of the behavior for the cookie notice functionality.
+     */
     init() {
 
         // Bind the click event for the "Accept" button
@@ -20,6 +29,9 @@ export class CookieNotice {
         }
     }
 
+    /**
+     * Binds the click event for the "Accept" button to its handler.
+     */
     bindAcceptButton() {
         let event = 'click.cookienotice',
             $acceptButton = $( '#wpcn_button_accept' );
@@ -28,14 +40,26 @@ export class CookieNotice {
             .on( event, () => this.handleAcceptClick() );
     }
 
+    /**
+     * Shows the cookie notice.
+     */
     showNotice() {
         this.noticeContainer.removeClass( 'd-none' );
     }
 
+    /**
+     * Hides the cookie notice.
+     */
     hideNotice() {
         this.noticeContainer.addClass( 'd-none' );
     }
 
+    /**
+     * Handler for when the "Accept" button is clicked.
+     * Hides the cookie notice and sets the related cookie so that the notice is not shown again.
+     *
+     * @param event
+     */
     handleAcceptClick( event ) {
         this.hideNotice();
         this.cookieManager.createCookie( this.cookieName, true, 365 );
